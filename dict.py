90e66d3a4ce13906b3efc5baa3cd13cd777723ef
ines-lorquet <ines.lorquet@laplateforme.io>
2025-12-04 15:51:50 +0100

    ajout du eda du dict du cleaning

---
+++ dict.py
@@ -0,0 +1,42 @@
+# Dictionnaire pour les lettres (ex: Score énergétique)
+mapping_scores = {
+    'A': 7,
+    'B': 6,
+    'C': 5,
+    'D': 4,
+    'E': 3,
+    'F': 2,
+    'G': 1
+}
+
+# Dictionnaire pour les types de logement
+mapping_types = {
+    1: 'Pisos',
+    2: 'Casa o chalet',
+    4: 'Dúplex',
+    5: 'Áticos'
+}
+
+# Dictionnaire pour les états du bien
+mapping_district ={
+    'Arganzuela': ['Imperial', 'Chopera', 'Acacias', 'Delicias', 'Palos de Moguer', 'Legazpi'],
+    'Barajas': ['Casco Histórico de Barajas', 'Alameda de Osuna', 'Timón', 'Campo de las Naciones-Corralejos'],
+    'Carabanchel': ['Opañel', 'Comillas', 'Abrantes', 'San Isidro', 'Puerta Bonita', 'Vista Alegre', 'Pau de Carabanchel', 'Buena Vista'],
+    'Centro': ['Lavapiés-Embajadores', 'Huertas-Cortes', 'Malasaña-Universidad', 'Chueca-Justicia', 'Palacio', 'Sol'],
+    'Chamartín': ['El Viso', 'Nueva España', 'Castilla', 'Bernabéu-Hispanoamérica', 'Prosperidad', 'Ciudad Jardín'],
+    'Chamberí': ['Gaztambide', 'Nuevos Ministerios-Ríos Rosas', 'Almagro', 'Trafalgar', 'Arapiles', 'Vallehermoso'],
+    'Ciudad Lineal': ['Ventas', 'Pueblo Nuevo', 'Quintana', 'San Juan Bautista', 'Colina', 'Costillares', 'Concepción', 'San Pascual'],
+    'Fuencarral': ['Las Tablas', 'Mirasierra', 'Montecarmelo', 'La Paz', 'Peñagrande', 'Pilar', 'Fuentelarreina', 'Tres Olivos - Valverde', 'Arroyo del Fresno'],
+    'Hortaleza': ['Sanchinarro', 'Conde Orgaz-Piovera', 'Valdebebas - Valdefuentes', 'Canillas', 'Palomas', 'Virgen del Cortijo - Manoteras', 'Pinar del Rey', 'Apóstol Santiago'],
+    'Latina': ['Puerta del Ángel', 'Aluche', 'Lucero', 'Los Cármenes', 'Águilas', 'Campamento'],
+    'Moncloa': ['Aravaca', 'Argüelles', 'Ciudad Universitaria', 'Valdezarza', 'Casa de Campo', 'Valdemarín', 'El Plantío'],
+    'Moratalaz': ['Fontarrón', 'Vinateros', 'Marroquina', 'Media Legua'],
+    'Puente de Vallecas': ['Palomeras sureste', 'Palomeras Bajas', 'San Diego', 'Entrevías', 'Numancia', 'Portazgo'],
+    'Retiro': ['Niño Jesús', 'Ibiza', 'Adelfas', 'Pacífico', 'Jerónimos', 'Estrella'],
+    'Salamanca': ['Recoletos', 'Goya', 'Lista', 'Castellana', 'Guindalera', 'Fuente del Berro'],
+    'Tetuán': ['Valdeacederas', 'Cuzco-Castillejos', 'Cuatro Caminos', 'Bellas Vistas', 'Berruguete', 'Ventilla-Almenara'],
+    'Usera': ['Orcasitas', 'San Fermín', 'Moscardó', 'Pradolongo', 'Zofío', 'Almendrales', '12 de Octubre-Orcasur'],
+    'Vicálvaro': ['Valdebernardo - Valderribas', 'Ambroz', 'Casco Histórico de Vicálvaro', 'El Cañaveral - Los Berrocales'],
+    'Villa de Vallecas': ['Casco Histórico de Vallecas', 'Ensanche de Vallecas - La Gavia', 'Santa Eugenia'],
+    'Villaverde': ['San Cristóbal', 'Los Ángeles', 'San Andrés', 'Los Rosales', 'Butarque']
+}
